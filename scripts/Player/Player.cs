4119f20d59853c9a4639ca7fc629ad4b8379a5a5
Shane Lillie <ignignokterr@gmail.com>
2024-01-03 16:58:19 -0800

    Very basic thing that moves around

---
+++ scripts/Player/Player.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+public partial class Player : CharacterBody3D
+{
+    [Export]
+    private float _speed = 5.0f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var input = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        Velocity = new Vector3(input.X, Velocity.Y, input.Y) * _speed;
+
+        MoveAndSlide();
+    }
+}
